14c8980815d023a0a3e0bae5555fa659c6a79bb6
almanybasja <149967867+almanybasja@users.noreply.github.com>
2024-01-09 16:33:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://graph.org/file/d7f33299ed8c60ad82721.mp4")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/ab0ab96a124df14bcf4ec.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Shahm_Sport")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ql8ql")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EGEEJ")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EGEEJ")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6066647930").split()))
 
 
-FAILED = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+FAILED = "https://telegra.ph/file/ab0ab96a124df14bcf4ec.jpg"
